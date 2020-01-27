cce2a1332b7a60a4b1ac62541394d87c71457bee
Serg <asdfg111000@yandex.ru>
2020-01-28 00:09:43 +0500

    Refactor code due to mentors notes, declare some variables as constants, change logic of main functions

--- js/stat.js
+++ js/stat.js
@@ -3,82 +3,81 @@
 var CLOUD_COORDS = [100, 10];
 var CLOUD_HEIGHT = 270;
 var CLOUD_WIDTH = 420;
+var CLOUD_COLOR = '#fff';
+var CLOUD_MESSAGE = 'Ура вы победили!';
+var CLOUD_PADDING = 10;
 var SHADOW_SHIFT = 10;
-var WINNER_MESSAGE = ['Ура вы победили!', 'Список результатов: '];
-var CLOUD_PADDING = 12;
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var CHART_TITLE = 'Список результатов:';
 var CHART_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_DISTANCE = 50;
+var FONT = '16px PT Mono';
+var FONT_COLOR = 'black';
+var TEXT_SHIFT = 15;
+var PLAYER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
+var PLAYER_NAME = 'Вы';
 
-var renderRect = function (ctx, x, y, width, height, color) {
+var drawRect = function (ctx, x, y, width, height, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 };
 
-var drawWinnerText = function (ctx, text, font, x, y, color, textShift) {
-  ctx.font = font;
-  ctx.fillStyle = color;
+var drawText = function (ctx, text, x, y) {
+  ctx.font = FONT;
+  ctx.fillStyle = FONT_COLOR;
   ctx.textBaseline = 'hanging';
-  var fontSize = getFontSize(font);
-  var lineSpacing = textShift + fontSize;
-  for (var i = 0; i < text.length; i++) {
-    ctx.fillText(text[i], x, y);
-    y += lineSpacing;
-  }
-  return y;
+  ctx.fillText(text, x, y);
 };
 
-var getFontSize = function (font) {
-  return parseInt(font.match(/\d+/)[0], 10);
-};
+var drawChart = function (ctx, names, times) {
+  var maxTime = getMaxElement(times);
+  var barCoordX = CLOUD_COORDS[0] + CLOUD_PADDING;
 
-var drawChart = function (ctx, names, times, barCoordX, barCoordY, chartHeight, labelFont, labelFontColor) {
-  var maxTime = getMaxTime(times);
-  var barHeight = 0;
-  var fontSize = getFontSize(labelFont);
-  barCoordX += CLOUD_PADDING;
-  var barColor = '';
-  for (var i = names.length - 1; i >= 0; i--) {
-    barHeight = Math.round((times[i] / maxTime) * chartHeight);
-    barColor = generateBarColor(names[i]);
-    renderRect(ctx, barCoordX, (barCoordY + chartHeight - barHeight + fontSize + CLOUD_PADDING), BAR_WIDTH, barHeight, barColor);
-    drawWinnerText(ctx, [Math.round(times[i]), names[i]], labelFont, barCoordX, (barCoordY + chartHeight - barHeight), labelFontColor, (barHeight + fontSize + CLOUD_PADDING));
+  for (var i = 0; i < names.length; i++) {
+    var barHeight = Math.round((times[i] / maxTime) * CHART_HEIGHT);
+    var barColor = (names[i] === PLAYER_NAME) ? PLAYER_BAR_COLOR : getRandomBlueHslColor();
+    var barCoordY = CLOUD_HEIGHT - CLOUD_PADDING * 2 - TEXT_SHIFT * 2 - barHeight;
+
+    drawText(ctx, Math.round(times[i]), barCoordX, barCoordY);
+    barCoordY += TEXT_SHIFT + CLOUD_PADDING;
+    drawRect(ctx, barCoordX, barCoordY, BAR_WIDTH, barHeight, barColor);
+    barCoordY += barHeight + CLOUD_PADDING;
+    drawText(ctx, names[i], barCoordX, barCoordY);
     barCoordX += BAR_WIDTH + BAR_DISTANCE;
   }
 };
 
-var generateBarColor = function (playerName) {
+var getRandomBlueHslColor = function () {
   var minSaturation = 10;
   var minLightness = 10;
   var maxSaturation = 100;
   var maxLightness = 100;
-  if (playerName === 'Вы') {
-    return 'rgba(255, 0, 0, 1)';
-  } else {
-    return 'hsl(230, ' + getRandomIntInclusive(minSaturation, maxSaturation) + '%, ' + getRandomIntInclusive(minLightness, maxLightness) + '%)';
-  }
+
+  return 'hsl(230, ' + getRandomIntInclusive(minSaturation, maxSaturation) + '%, ' + getRandomIntInclusive(minLightness, maxLightness) + '%)';
 };
 
-var getMaxTime = function (times) {
-  var maxTime = 0;
-  for (var i = 0; i < times.length; i++) {
-    maxTime = (maxTime < times[i]) ? times[i] : maxTime;
+var getMaxElement = function (elements) {
+  var max = 0;
+
+  for (var i = 0; i < elements.length; i++) {
+    max = (max < elements[i]) ? elements[i] : max;
   }
-  return maxTime;
+
+  return max;
 };
 
 var getRandomIntInclusive = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  var labelFont = '16px PT Mono';
-  var labelFontColor = 'black';
-  var headerTextShift = 5;
-  renderRect(ctx, CLOUD_COORDS[0] + SHADOW_SHIFT, CLOUD_COORDS[1] + SHADOW_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
-  renderRect(ctx, CLOUD_COORDS[0], CLOUD_COORDS[1], CLOUD_WIDTH, CLOUD_HEIGHT, '#fff');
-  var barCoordY = drawWinnerText(ctx, WINNER_MESSAGE, labelFont, CLOUD_COORDS[0] + CLOUD_PADDING, CLOUD_COORDS[1] + CLOUD_PADDING, labelFontColor, headerTextShift);
-  drawChart(ctx, names, times, CLOUD_COORDS[0], barCoordY, CHART_HEIGHT, labelFont, labelFontColor);
+  drawRect(ctx, CLOUD_COORDS[0] + SHADOW_SHIFT, CLOUD_COORDS[1] + SHADOW_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
+  drawRect(ctx, CLOUD_COORDS[0], CLOUD_COORDS[1], CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
+  drawText(ctx, CLOUD_MESSAGE, CLOUD_COORDS[0] + CLOUD_PADDING, CLOUD_COORDS[1] + CLOUD_PADDING);
+  drawText(ctx, CHART_TITLE, CLOUD_COORDS[0] + CLOUD_PADDING, CLOUD_COORDS[1] + CLOUD_PADDING + TEXT_SHIFT);
+  drawChart(ctx, names, times);
 };
