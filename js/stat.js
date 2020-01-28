8a9d28a866ea70bdde695f8079780557eebf58dc
Serg <asdfg111000@yandex.ru>
2020-01-28 21:29:23 +0500

    Add constants instead of variables, rename some variables

--- js/stat.js
+++ js/stat.js
@@ -5,10 +5,12 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_WIDTH = 420;
 var CLOUD_COLOR = '#fff';
 var CLOUD_MESSAGE = 'Ура вы победили!';
+var CLOUD_MESSAGE_COORDS = [110, 30];
 var CLOUD_PADDING = 10;
-var SHADOW_SHIFT = 10;
+var SHADOW_COORDS = [110, 20];
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var CHART_TITLE = 'Список результатов:';
+var CHART_TITLE_COORDS = [110, 45];
 var CHART_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_DISTANCE = 50;
@@ -32,19 +34,19 @@ var drawText = function (ctx, text, x, y) {
 
 var drawChart = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
-  var barCoordX = CLOUD_COORDS[0] + CLOUD_PADDING;
+  var sectionCoordX = CLOUD_COORDS[0] + CLOUD_PADDING;
 
   for (var i = 0; i < names.length; i++) {
     var barHeight = Math.round((times[i] / maxTime) * CHART_HEIGHT);
     var barColor = (names[i] === PLAYER_NAME) ? PLAYER_BAR_COLOR : getRandomBlueHslColor();
-    var barCoordY = CLOUD_HEIGHT - CLOUD_PADDING * 2 - TEXT_SHIFT * 2 - barHeight;
+    var timeCoordY = CLOUD_HEIGHT - CLOUD_PADDING * 2 - TEXT_SHIFT * 2 - barHeight;
 
-    drawText(ctx, Math.round(times[i]), barCoordX, barCoordY);
-    barCoordY += TEXT_SHIFT + CLOUD_PADDING;
-    drawRect(ctx, barCoordX, barCoordY, BAR_WIDTH, barHeight, barColor);
-    barCoordY += barHeight + CLOUD_PADDING;
-    drawText(ctx, names[i], barCoordX, barCoordY);
-    barCoordX += BAR_WIDTH + BAR_DISTANCE;
+    drawText(ctx, Math.round(times[i]), sectionCoordX, timeCoordY);
+    var barCoordY = timeCoordY + TEXT_SHIFT + CLOUD_PADDING;
+    drawRect(ctx, sectionCoordX, barCoordY, BAR_WIDTH, barHeight, barColor);
+    var nameCoordY = barCoordY + barHeight + CLOUD_PADDING;
+    drawText(ctx, names[i], sectionCoordX, nameCoordY);
+    sectionCoordX += BAR_WIDTH + BAR_DISTANCE;
   }
 };
 
@@ -53,8 +55,10 @@ var getRandomBlueHslColor = function () {
   var minLightness = 10;
   var maxSaturation = 100;
   var maxLightness = 100;
+  var saturation = getRandomIntInclusive(minSaturation, maxSaturation);
+  var lightness = getRandomIntInclusive(minLightness, maxLightness);
 
-  return 'hsl(230, ' + getRandomIntInclusive(minSaturation, maxSaturation) + '%, ' + getRandomIntInclusive(minLightness, maxLightness) + '%)';
+  return 'hsl(230, ' + saturation + '%, ' + lightness + '%)';
 };
 
 var getMaxElement = function (elements) {
@@ -75,9 +79,9 @@ var getRandomIntInclusive = function (min, max) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  drawRect(ctx, CLOUD_COORDS[0] + SHADOW_SHIFT, CLOUD_COORDS[1] + SHADOW_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
+  drawRect(ctx, SHADOW_COORDS[0], SHADOW_COORDS[1], CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
   drawRect(ctx, CLOUD_COORDS[0], CLOUD_COORDS[1], CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
-  drawText(ctx, CLOUD_MESSAGE, CLOUD_COORDS[0] + CLOUD_PADDING, CLOUD_COORDS[1] + CLOUD_PADDING);
-  drawText(ctx, CHART_TITLE, CLOUD_COORDS[0] + CLOUD_PADDING, CLOUD_COORDS[1] + CLOUD_PADDING + TEXT_SHIFT);
+  drawText(ctx, CLOUD_MESSAGE, CLOUD_MESSAGE_COORDS[0], CLOUD_MESSAGE_COORDS[1]);
+  drawText(ctx, CHART_TITLE, CHART_TITLE_COORDS[0], CHART_TITLE_COORDS[1]);
   drawChart(ctx, names, times);
 };
