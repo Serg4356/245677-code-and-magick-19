d465b9c45d0b96e7e62f4618037531d2f46ce4c2
Serg <asdfg111000@yandex.ru>
2020-01-28 23:09:15 +0500

    Return roundings to random generator function

--- js/stat.js
+++ js/stat.js
@@ -78,7 +78,10 @@ var getMaxElement = function (elements) {
 };
 
 var getRandomIntInclusive = function (min, max) {
-  return Math.round(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 window.renderStatistics = function (ctx, names, times) {
