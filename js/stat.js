315296a294ce0f23e8c3b6e18b679ff8174bfb2b
Serg <asdfg111000@yandex.ru>
2020-01-28 22:28:37 +0500

    Replace magic number with constant, refactor code

--- js/stat.js
+++ js/stat.js
@@ -1,16 +1,21 @@
 'use strict';
 
-var CLOUD_COORDS = [100, 10];
+var CLOUD_COORD_X = 100;
+var CLOUD_COORD_Y = 10;
 var CLOUD_HEIGHT = 270;
 var CLOUD_WIDTH = 420;
 var CLOUD_COLOR = '#fff';
 var CLOUD_MESSAGE = 'Ура вы победили!';
-var CLOUD_MESSAGE_COORDS = [110, 30];
-var CLOUD_PADDING = 10;
-var SHADOW_COORDS = [110, 20];
+var CLOUD_MESSAGE_COORD_X = 110
+var CLOUD_MESSAGE_COORD_Y = 30;
+var ELEMENT_SPACING = 10;
+var SHADOW_COORD_X = 110
+var SHADOW_COORD_Y = 20;
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var CHART_TITLE = 'Список результатов:';
-var CHART_TITLE_COORDS = [110, 45];
+var CHART_TITLE_COORD_X = 110;
+var CHART_TITLE_COORD_Y = 45;
+var CHART_SECTION_LABEL_COUNT = 2;
 var CHART_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_DISTANCE = 50;
@@ -34,17 +39,18 @@ var drawText = function (ctx, text, x, y) {
 
 var drawChart = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
-  var sectionCoordX = CLOUD_COORDS[0] + CLOUD_PADDING;
+  var sectionCoordX = CLOUD_MESSAGE_COORD_X;
+
 
   for (var i = 0; i < names.length; i++) {
     var barHeight = Math.round((times[i] / maxTime) * CHART_HEIGHT);
     var barColor = (names[i] === PLAYER_NAME) ? PLAYER_BAR_COLOR : getRandomBlueHslColor();
-    var timeCoordY = CLOUD_HEIGHT - CLOUD_PADDING * 2 - TEXT_SHIFT * 2 - barHeight;
+    var timeCoordY = CLOUD_HEIGHT - (ELEMENT_SPACING + TEXT_SHIFT) * CHART_SECTION_LABEL_COUNT - barHeight;
 
     drawText(ctx, Math.round(times[i]), sectionCoordX, timeCoordY);
-    var barCoordY = timeCoordY + TEXT_SHIFT + CLOUD_PADDING;
+    var barCoordY = timeCoordY + TEXT_SHIFT + ELEMENT_SPACING;
     drawRect(ctx, sectionCoordX, barCoordY, BAR_WIDTH, barHeight, barColor);
-    var nameCoordY = barCoordY + barHeight + CLOUD_PADDING;
+    var nameCoordY = barCoordY + barHeight + ELEMENT_SPACING;
     drawText(ctx, names[i], sectionCoordX, nameCoordY);
     sectionCoordX += BAR_WIDTH + BAR_DISTANCE;
   }
@@ -79,9 +85,9 @@ var getRandomIntInclusive = function (min, max) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  drawRect(ctx, SHADOW_COORDS[0], SHADOW_COORDS[1], CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
-  drawRect(ctx, CLOUD_COORDS[0], CLOUD_COORDS[1], CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
-  drawText(ctx, CLOUD_MESSAGE, CLOUD_MESSAGE_COORDS[0], CLOUD_MESSAGE_COORDS[1]);
-  drawText(ctx, CHART_TITLE, CHART_TITLE_COORDS[0], CHART_TITLE_COORDS[1]);
+  drawRect(ctx, SHADOW_COORD_X, SHADOW_COORD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
+  drawRect(ctx, CLOUD_COORD_X, CLOUD_COORD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
+  drawText(ctx, CLOUD_MESSAGE, CLOUD_MESSAGE_COORD_X, CLOUD_MESSAGE_COORD_Y);
+  drawText(ctx, CHART_TITLE, CHART_TITLE_COORD_X, CHART_TITLE_COORD_Y);
   drawChart(ctx, names, times);
 };
